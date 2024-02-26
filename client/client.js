2003572c2ba191ae21633ffd0e57087a9e709197
GabrielAdrian348 <gr2896960gmail.com>
2024-02-25 22:18:21 -0300

    Creación de cliente

---
+++ client/client.js
@@ -0,0 +1,14 @@
+import net from "node:net";
+import dotenv from "dotenv";
+dotenv.config();
+
+const options = {
+    port: process.env.PORT ?? 2323,
+    host: process.env.HOST ?? "localhost",
+};
+
+const clientTCP = net.connect(options);
+
+clientTCP.on("connect", () => {
+    console.log("Cliente conectado");
+});
